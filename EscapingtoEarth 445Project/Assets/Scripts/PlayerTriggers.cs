b14b14f4fe2f6141c086315af816abdbc8520947
AhmedHAbdel <id99tk15@ubalt.edu>
2023-11-09 15:20:37 -0500

    Ground Enemy aI
    Detection
    Death on impact
    Raycast DrawLine
    DebugLog for detection

--- EscapingtoEarth 445Project/Assets/Scripts/PlayerTriggers.cs
+++ EscapingtoEarth 445Project/Assets/Scripts/PlayerTriggers.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerTriggers : MonoBehaviour
 {
@@ -22,6 +23,7 @@ public class PlayerTriggers : MonoBehaviour
         {
             Destroy(player);
             Debug.Log("Ouch");
+            SceneManager.LoadScene(0);
         }
     }
 }
