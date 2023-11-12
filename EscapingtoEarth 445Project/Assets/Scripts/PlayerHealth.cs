c809924dcfdcd80b4a518dde276ba020ea54ad0c
dagray0524 <devone.gray@ubalt.edu>
2023-11-12 03:42:40 -0500

    Pause menu and Health Bar
    I was able to create a Pause Menu. I explain more on Discord. Thank you.

--- EscapingtoEarth 445Project/Assets/Scripts/PlayerHealth.cs
+++ EscapingtoEarth 445Project/Assets/Scripts/PlayerHealth.cs
@@ -20,7 +20,7 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             TakeDamage(20);
         }
