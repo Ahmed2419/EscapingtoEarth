7881b5eeb56b125273a2dc9027e80a4236efacd5
cornbob777 <148503703+cornbob777@users.noreply.github.com>
2023-12-10 17:37:59 -0500

    added destroy bullets

--- EscapingtoEarth 445Project/Assets/Scripts/EnemeyBullet.cs
+++ EscapingtoEarth 445Project/Assets/Scripts/EnemeyBullet.cs
@@ -20,6 +20,6 @@ public class EnemeyBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy (this.gameObject, 3);
     }
 }
--- EscapingtoEarth 445Project/Assets/Scripts/PlayerBullet.cs
+++ EscapingtoEarth 445Project/Assets/Scripts/PlayerBullet.cs
@@ -18,6 +18,6 @@ public class PlayerBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy (this.gameObject, 3);
     }
 }
